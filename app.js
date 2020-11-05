4089319b69db5e901b83ddf58c54f7791702a1d2
rhmnaulia <rahmanauliax@gmail.com>
2020-11-05 22:03:08 +0700

    Add POST method

--- app.js
+++ app.js
@@ -8,11 +8,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
-	console.log('Listening to port 3000');
-});
-
-mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true });
+mongoose
+	.connect('mongodb://localhost:27017/wikiDB', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('DB Connected!'))
+	.catch((err) => {
+		console.log(err);
+	});
 
 const articleSchema = {
 	title: String,
@@ -23,6 +27,32 @@ const Article = mongoose.model('Article', articleSchema);
 
 app.get('/articles', (req, res) => {
 	Article.find((err, foundArticles) => {
-		res.send(foundArticles);
+		if (err) {
+			res.send(err);
+		} else {
+			res.send(foundArticles);
+		}
 	});
+});
+
+app.post('/articles', (req, res) => {
+	console.log(req.body.title);
+	console.log(req.body.content);
+
+	const newArticle = new Article({
+		title: req.body.title,
+		content: req.body.content,
+	});
+
+	newArticle.save((err) => {
+		if (err) {
+			res.send(err);
+		} else {
+			res.send('Succesfully added new article.');
+		}
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Listening to port 3000');
 });
